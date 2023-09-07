1c11eec61e0bf1c631c686d1346b5582f581661b
OsMax <www.osmax@gmail.com>
2023-09-07 12:44:50 +0300

    propTypes

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import css from './ContactList.module.css';
 
 const ContactList = ({ contacts, onDeleteContact }) => {
@@ -22,6 +23,17 @@ const ContactList = ({ contacts, onDeleteContact }) => {
       </ul>
     </div>
   );
+};
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+  onDeleteContact: PropTypes.func.isRequired,
 };
 
 export default ContactList;
